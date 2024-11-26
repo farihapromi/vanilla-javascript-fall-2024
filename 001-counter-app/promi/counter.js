2559f80c9a35a0f22e37ec32394cc80eca4af319
Fariha Promi <farihapromi2611@gmail.com>
2024-11-26 12:12:58 +0600

    file moved to promi

--- 001-counter-app/promi/counter.js
+++ 001-counter-app/promi/counter.js
@@ -4,14 +4,25 @@ const incrementButton = document.getElementById("increment");
 const decrementButton = document.getElementById("decrement");
 
 const increaseCount = () => {
-  count = count + 1;
+  if (count >= 10) {
+    alert(`Count can not be greater or equal to 10.OverFlow problem`);
+    return;
+  } else {
+    count = count + 1;
+  }
   counterElement.innerText = count;
 };
 
 incrementButton.addEventListener("click", increaseCount);
 
 const decreaseCount = () => {
-  count = count - 1;
+  if (count == 0) {
+    alert(`underflow`);
+    return;
+  } else {
+    count = count - 1;
+  }
+
   counterElement.innerText = count;
 };
 decrementButton.addEventListener("click", decreaseCount);
