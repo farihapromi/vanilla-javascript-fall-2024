31247177283568735e3bc6ddcf73316d4a11b014
Fariha Promi <farihapromi2611@gmail.com>
2024-11-26 13:45:42 +0600

    comparison function added

---
+++ 002-number-comparator/bongodev/number-comparator.js
@@ -0,0 +1,23 @@
+// Storing input id
+const num1Input = document.getElementById("number1");
+const num2Input = document.getElementById("number2");
+const result = document.getElementById("result");
+const button = document.getElementById("compare");
+
+const handleButton = () => {
+  // Retrieve values from input fields
+  const num1 = parseFloat(num1Input.value);
+  const num2 = parseFloat(num2Input.value);
+  // Validate input
+  if (isNaN(num1) || isNaN(num2)) {
+    resultElement.textContent = "Please enter valid numbers.";
+    return;
+  }
+  if (num1 > num2) {
+    result.innerText = `First Number is larger than Second Number`;
+  } else {
+    result.innerText = `Second  Number is larger than  First Number`;
+  }
+};
+
+button.addEventListener("click", handleButton);
